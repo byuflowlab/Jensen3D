a54c38d6f9dd347d6fff867c6968d4d9cec937f7
PJ Stanley <stanley_andrewpj@yahoo.com>
2016-03-17 13:54:08 -0600

    My starting Jensen model and what I have done so far with openMDAO

---
+++ Jensen.py
@@ -0,0 +1,195 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+from math import pi
+from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.art3d as art3d
+from matplotlib.patches import Circle
+
+def Jensen_Wake_Model(xHAWT, yHAWT, zHAWT, params):
+    theta = 0.1
+    alpha = sp.tan(theta)
+    rho = 1.1716
+    a = 1. / 3.
+    Cp = 4.*a*(1-a)**2.
+    # nTurbines = len(xin)/2.
+    r_0, U_velocity = params
+    "Make the graphic for the turbines and wakes"
+    # jensen_plot(x, y, r_0, alpha, U_direction_radians)
+
+    "Calculate power from each turbine"
+    return jensen_power(xHAWT, yHAWT, zHAWT, r_0, alpha, a, U_velocity, rho, Cp)
+
+    # plt.show()
+
+
+#Determine how much of the turbine is in the wake of the other turbines
+def overlap(x, xdown, y, ydown, z, zdown, r, rdown, alpha):
+    overlap_fraction = np.zeros(np.size(x))
+    for i in range(0, np.size(x)):
+        #define dx as the upstream x coordinate - the downstream x coordinate then rotate according to wind direction
+        dx = xdown - x[i]
+        #define dy as the upstream y coordinate - the downstream y coordinate then rotate according to wind direction
+        dy = abs(ydown - y[i])
+        dz = abs(zdown - z[i])
+        d = sp.sqrt(dy**2.+dz**2.)
+        R = r[i]+dx*alpha #The radius of the wake depending how far it is from the turbine
+        A = rdown**2*pi #The area of the turbine
+        if dx > 0:
+            #if d <= R-rdown:
+            #    overlap_fraction[i] = 1 #if the turbine is completely in the wake, overlap is 1, or 100%
+            if d == 0:
+                print "Area of turbine: ", A
+                print "Area of wake: ", pi*R**2
+                if A <= pi*R**2:
+                    overlap_fraction[i] = 1.
+                else: 
+                    overlap_fraction[i] = pi*R**2/A
+            elif d >= R+rdown:
+                overlap_fraction[i] = 0 #if none of it touches the wake, the overlap is 0
+            else:
+                #if part is in and part is out of the wake, the overlap fraction is defied by the overlap area/rotor area
+                overlap_area = rdown**2.*sp.arccos((d**2.+rdown**2.-R**2.)/(2.0*d*rdown))+R**2.*sp.arccos((d**2.+R**2.-rdown**2.)/(2.0*d*R))-0.5*sp.sqrt((-d+rdown+R)*(d+rdown-R)*(d-rdown+R)*(d+rdown+R))
+                overlap_fraction[i] = overlap_area/A
+        else:
+            overlap_fraction[i] = 0 #turbines cannot be affected by any wakes that start downstream from them
+
+    # print overlap_fraction
+    print overlap_fraction
+    return overlap_fraction #retrun the n x n matrix of how each turbine is affected by all of the others
+                            #for example [0, 0.5]
+                                        #[0, 0] means that the first turbine (row one) has half of its area in the
+                                        #wake of the second turbine (row two). The overlap_fraction on the second
+                                        #turbine is zero, so we can conclude that it is upstream of the first
+
+
+#Jensen wake decay to determine the total velocity deficit at each turbine
+def loss(r_0, a, alpha, x_focus, x, overlap):
+    loss = np.zeros(np.size(x))
+    loss_squared = np.zeros(np.size(x))
+    dx = np.zeros(np.size(x))
+    for i in range(0, np.size(x)):
+        dx[i] = x_focus-x[i]
+        if dx[i] > 0:
+            loss[i] = overlap[i]*2.*a*(r_0[i]/(r_0[i]+alpha*(dx[i])))**2
+            loss_squared[i] = loss[i]**2
+        else:
+            loss[i] = 0
+            loss_squared[i] = 0
+    total_loss = sp.sqrt(np.sum(loss_squared))
+    return total_loss
+
+
+def jensen_power(x, y, z, r_0, alpha, a, U_velocity, rho, Cp):
+    "Effective velocity at each turbine"
+    V = np.zeros([np.size(x)])
+    total_loss = np.zeros([np.size(x)])
+    P = np.zeros([np.size(x)])
+    # print "\nOverlap Fraction Matrix"
+    for i in range(0, np.size(x)):
+        A = r_0[i]**2*pi
+        overlap_fraction = overlap(x, x[i], y, y[i], z, z[i], r_0, r_0[i], alpha)
+        total_loss[i] = loss(r_0, a, alpha, x[i], x, overlap_fraction)
+        V = (1-total_loss[i])*U_velocity
+        P[i] = 0.5*rho*A*Cp*V**3
+    # print V
+    # print "Total Loss: ", total_loss
+    "Calculate Power from each turbine and the total"
+    
+    
+
+    P_total = np.sum(P)
+
+    # print "\nPower Output From Each Turbine"
+    # print P
+    # print "\nTotal Power Output"
+    # print P_total
+    return P_total
+
+
+def jensen_plot(x, y, r_0, alpha, U_direction_radians):
+    #plt.plot([x], [y], 'ro', markersize=10)
+
+    wakes = np.linspace(0, 1000, num=101)
+
+    for i in range(0, np.size(y)):
+        turbine_y_top = y[i]+r_0
+        turbine_y_bottom = y[i]-r_0
+        turbine_x = [x[i], x[i], x[i]]
+        turbine_y = [turbine_y_bottom, y[i], turbine_y_top]
+        plt.plot(turbine_x, turbine_y, linewidth=2, c='r')
+        for j in range(1, np.size(wakes)):
+            wake_x = x[i]+wakes[j]
+            wake_top_y = y[i]+r_0+wakes[j]*alpha
+            wake_bottom_y = y[i]-r_0-wakes[j]*alpha
+            plt.plot(wake_x, wake_top_y, 'b.', markersize=2)
+            plt.plot(wake_x, wake_bottom_y, 'b.', markersize=2)
+
+
+def rotate(x, y, U_direction_radians):
+    x_r = x*sp.cos(U_direction_radians)-y*sp.sin(U_direction_radians)
+    y_r = x*sp.sin(U_direction_radians)+y*sp.cos(U_direction_radians)
+    return x_r, y_r
+
+
+if __name__ == '__main__':
+ 
+    "Define Variables"
+    theta = 0.1
+    alpha = sp.tan(theta)
+    x = np.array([0, 0, 0, 500, 500, 500, 1000, 1000, 1000]) #x coordinates of the turbines
+    y = np.array([0, 500, 1000, 0, 500, 1000, 0, 500, 1000]) #y coordinates of the turbines
+    z = np.array([150, 150, 150, 250, 250, 250, 350, 500, 350]) #hub height of each turbine
+    r_0 = np.array([40, 40, 40, 50, 50, 50, 60, 75, 60])
+    # r_0 = np.ones(len(x))*20
+    rho = 1.1716
+    a = 1. / 3.
+    U_velocity = 8.
+    "0 degrees is coming from due North. +90 degrees means the wind is coming from due East, -90 from due West"
+    U_direction = -90.
+    
+
+    U_direction_radians = (U_direction+90) * pi / 180.
+    #print U_direction_radians
+    Cp = 4.*a*(1-a)**2.
+
+    # x_r, y_r = rotate(x, y, U_direction_radians)
+
+    # Jensen_Wake_Model(overlap, loss, jensen_power, jensen_plot, x, y, r_0, alpha, U_direction_radians)
+    # xin = np.hstack([x, y])
+    params = (r_0, U_velocity)
+    print Jensen_Wake_Model(x, y, z, params)
+    ax = Axes3D(plt.gcf())
+    for i in range(len(x)):
+        ax.scatter(x[i], y[i], z[i], c = 'r', s=pi*r_0[i]**2, marker='.')
+        fillstyles = ('none')
+        #plt.axis(U_direction_radians)
+        xtemp = (x[i], x[i])
+        ytemp = (y[i], y[i])
+        ztemp = (0, z[i]-r_0[i])
+        ax.plot(xtemp, ytemp, ztemp, zdir='z', c='b', linewidth = 5.0)
+     
+    ax.set_xlim([0, np.max(x)])   
+    ax.set_ylim([0, np.max(y)])  
+    ax.set_zlim([0, np.max(z)])
+    plt.show()
+
+    # wakes = np.linspace(0, 1000, num=101)
+    #
+    # plt.figure(2)
+    # for i in range(0, np.size(y)):
+    #     turbine_x_top = ((wakes[0])*sp.cos(-U_direction_radians)-(r_0+alpha*wakes[0])*sp.sin(-U_direction_radians))+x[i]
+    #     turbine_y_top = ((wakes[0])*sp.sin(-U_direction_radians)+(r_0+alpha*wakes[0])*sp.cos(-U_direction_radians))+y[i]
+    #     turbine_x_bottom = ((wakes[0])*sp.cos(-U_direction_radians)-(-r_0-alpha*wakes[0])*sp.sin(-U_direction_radians))+x[i]
+    #     turbine_y_bottom = ((wakes[0])*sp.sin(-U_direction_radians)+(-r_0-alpha*wakes[0])*sp.cos(-U_direction_radians))+y[i]
+    #     turbine_x = [turbine_x_bottom, x[i], turbine_x_top]
+    #     turbine_y = [turbine_y_bottom, y[i], turbine_y_top]
+    #     plt.plot(turbine_x, turbine_y, linewidth=2, c='r')
+    #     for j in range(1, np.size(wakes)):
+    #         top_x = ((wakes[j])*sp.cos(-U_direction_radians)-(r_0+alpha*wakes[j])*sp.sin(-U_direction_radians))+x[i]
+    #         top_y = ((wakes[j])*sp.sin(-U_direction_radians)+(r_0+alpha*wakes[j])*sp.cos(-U_direction_radians))+y[i]
+    #         bottom_x = ((wakes[j])*sp.cos(-U_direction_radians)-(-r_0-alpha*wakes[j])*sp.sin(-U_direction_radians))+x[i]
+    #         bottom_y = ((wakes[j])*sp.sin(-U_direction_radians)+(-r_0-alpha*wakes[j])*sp.cos(-U_direction_radians))+y[i]
+    #         plt.plot([top_x], [top_y], 'b.', markersize=2)
+    #         plt.plot([bottom_x], [bottom_y], 'b.', markersize=2)
+    # plt.show()
---
+++ JensenOpenMDAO.py
@@ -0,0 +1,115 @@
+from __future__ import print_function
+from math import tan, pi, sqrt, acos
+import numpy as np
+from openmdao.api import IndepVarComp, Component, Problem, Group
+from openmdao.api import ScipyOptimizer
+		
+
+
+class wakeOverlap(Component):
+	
+
+	def __init__(self, nTurbs):
+		super(wakeOverlap, self).__init__()
+
+		self.fd_options['form'] = 'central'
+		self.fd_options['step_size'] = 1.0e-6
+		self.fd_options['step_type'] = 'relative'
+
+		self.nTurbs = nTurbs
+		self.add_param('x', val=np.zeros(nTurbs))
+		self.add_param('y', val=np.zeros(nTurbs))
+		self.add_param('z', val=np.zeros(nTurbs))
+		self.add_param('r', val=np.zeros(nTurbs))
+		self.add_param('alpha', val=tan(0.1))
+
+		self.add_output('overlap', val=np.eye(nTurbs))
+
+	def solve_nonlinear(self, params, unknowns, resids):
+		
+		x = params['x']
+		y = params['y']
+		z = params['z']
+		r = params['r']
+		alpha = params['alpha']
+		nTurbs = len(x)
+		
+		overlap_fraction = np.eye(nTurbs)
+		for i in range(nTurbs):
+			for j in range(nTurbs):
+				dx = x[i]-x[j]
+				dy = abs(y[i]-y[j])
+				dz = abs(z[i]-z[j])
+				d = sqrt(dy**2+dz**2)
+				R = r[j]+dx*alpha
+				A = r[i]**2*pi
+				
+				if dx <= 0:
+					overlap_fraction[i][j] = 0.0
+				else:
+					if d <= R-r[i]:
+						if A <= pi*R**2:
+							overlap_fraction[i][j] = 1.0
+						else: 
+							overlap_fraction[i][j] = pi*R**2/A
+					elif d >= R+r[i]:
+						overlap_fraction[i][j] = 0.0
+					else:
+						overlap_area = r[i]**2.*acos((d**2.+r[i]**2.-R**2.)/(2.0*d*r[i]))+R**2.*aos((d**2.+R**2.-r[i]**2.)/(2.0*d*R))-0.5*sqrt((-d+r[i]+R)*(d+r[i]-R)*(d-r[i]+R)*(d+r[i]+R))
+                overlap_fraction[i][j] = overlap_area/A
+				
+				
+		unknowns['overlap'] = overlap_fraction
+
+
+
+class jensenPower(Component):
+	
+
+	def __init__(self, nTurbs):
+		super(jensenPower, self).__init__()
+		
+		self.fd_options['form'] = 'central'
+		self.fd_options['step_size'] = 1.0e-6
+		self.fd_options['step_type'] = 'relative'
+		
+		self.nTurbs = nTurbs
+		self.add_param('x', val=np.zeros(nTurbs))
+		self.add_param('r', val=np.zeros(nTurbs))
+		self.add_param('alpha', val=tan(0.1))
+		self.add_param('windSpeed', val=0.0)
+		self.add_param('rho', val=0.0)
+		self.add_param('a', val=1./3.)
+		self.add_param('Cp', val=4.*a*(1-a)**2.)
+		self.add_param('overlap', val=np.empty([nTurbs, nTurbs]))
+
+		self.add_output('Power', val=np.zeros(nTurbs))
+
+	def solve_nonlinear(self, params, unknowns, resids):
+	
+		x = params['x']
+		r = params['r']
+		alpha = params['alpha']
+		a = params['a']
+		windSpeed = params['windSpeed']
+		self.add_param('rho', val=1.1716)
+		Cp = params['Cp']
+		nTurbs = len(x)
+		loss = np.zeros(nTurbs)
+		power = np.zeros(nTurbs)
+		overlap = params['overlap']
+	
+		for i in range(nTurbs):
+			for j in range(nTurbs):
+				dx = x[i]-x[j]
+				loss[j] = overlap[i][j]*2.0*a*(r[j]/(r[j]+alpha*dx))**2
+				loss[j] = loss[j]**2
+			totalLoss = sqrt(np.sum(loss))
+			effectiveVelocity = (1-totalLoss)*windSpeed
+			A = pi*r[i]**2
+			power[i] = 0.5*rho*A*Cp*effectiveVelocity**3
+		unknowns['Power'] = np.sum(power)
+
+
+
+
