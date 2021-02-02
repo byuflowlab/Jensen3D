3281b88738d67a677672db157b40f5fca31d5b9a
jaredthomas68 <jaredthomas68@gmail.com>
2021-02-02 11:00:04 -0700

    adjust for correct gradient compiling

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ from numpy.distutils.core import setup, Extension
 
 module1 = Extension('_jensen', sources=['src/jensen3d/jensen.f90'], extra_compile_args=['-O2', '-c'])
 module2 = Extension('_jensen2', sources=['src/jensen3d/jensencosineWEC.f90'], extra_compile_args=['-O2', '-c'])
-module3 = Extension('_jensen2diff', sources=['src/jensen3d/jensencosineWEC_bv.f90'], extra_compile_args=['-O2', '-c'])
+module3 = Extension('_jensen2diff', sources=['src/jensen3d/jensencosineWEC_bv.f90',
+                                            'src/jensen3d/adBuffer.f',
+                                            'src/jensen3d/adStack.c'], extra_compile_args=['-O2', '-c'])
 
 setup(
     name='Jensen3D',
